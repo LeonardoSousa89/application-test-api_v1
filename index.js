175251968e7521e76f97c4e81e37b2cc9984ba9f
LeonardoMendes89 <leoprogressivo@hotmail.com>
2022-04-11 23:46:08 -0300

    master

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const express   = require('express')
 const app       = express()
 
 app.use(cors({ methods:['GET','POST','PUT','DELETE'],
-               origin: 'https://anotation-app.herokuapp.com'
+               origin: 'https://anotation-app.herokuapp.com/'
 }))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
