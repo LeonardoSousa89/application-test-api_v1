c246aa6c91e445d36269aba8c8d30218ef7b83b8
LeonardoMendes89 <leoprogressivo@hotmail.com>
2022-04-12 00:50:22 -0300

    master

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const log       = require('morgan')
 const express   = require('express')
 const app       = express()
 
-app.use(cors())
+app.use(cors('*'))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(log('dev'))
