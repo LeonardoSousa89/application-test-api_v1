e7e1d983347160161b76694e2a0a20784939206a
LeonardoMendes89 <leoprogressivo@hotmail.com>
2022-04-12 11:16:56 -0300

    master

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const log       = require('morgan')
 const express   = require('express')
 const app       = express()
 
-app.use(cors({  origin:'https://anotation-app.herokuapp.com' }))
+app.use(cors({  origin:['https://anotation-app.herokuapp.com','https://anotation-app.herokuapp.com/app/login'] }))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(log('dev'))
