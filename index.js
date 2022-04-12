f2bf073e7be6e0e60eb11c64421f877c384f9b9c
LeonardoMendes89 <leoprogressivo@hotmail.com>
2022-04-12 00:23:35 -0300

    master

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const log       = require('morgan')
 const express   = require('express')
 const app       = express()
 
-app.use(cors({ origin:'*'
+app.use(cors({ methods:['GET','POST','PUT','DELETE'],
+               origin:'https://anotation-app.herokuapp.com'
 }))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
@@ -17,4 +18,3 @@ app.listen(process.env.PORT || _port,()=>{
     console.log(`localhost:${_port}`)
 })
 
-// methods:['GET','POST','PUT','DELETE'],
